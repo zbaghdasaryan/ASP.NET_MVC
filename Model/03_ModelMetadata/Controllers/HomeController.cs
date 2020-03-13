c01e63ab4904526da7341c62d298ec07fe00e864
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2020-03-13 18:56:48 +0400

    ModelMetadata

---
+++ Model/03_ModelMetadata/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using _03_ModelMetadata.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _03_ModelMetadata.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Customer customer = new Customer()
+            {
+                Id = 1,
+                FirstName = "Poghos",
+                LastName = "Poghosyan",
+                IsApproved = true,
+                Description = "text....text...text",
+                RegistrationDate = DateTime.Now,
+                Foo = 123
+            };
+            return View(customer);
+        }
+    }
+}
