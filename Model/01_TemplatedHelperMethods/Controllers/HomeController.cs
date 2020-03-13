3a551a884b283eeb8fd7ff69cebb7cee41793c96
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2020-03-13 18:04:45 +0400

    TemplatedHelperMethods

---
+++ Model/01_TemplatedHelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using _01_TemplatedHelperMethods.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _01_TemplatedHelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Product product = new Product()
+            { 
+            Id=1,
+            Name="Test Product",
+            IsTangible=false,
+            };
+            
+            return View(product);
+        }
+
+       
+    }
+}
