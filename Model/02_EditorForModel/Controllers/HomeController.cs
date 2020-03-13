994436f6a719c6bf688d8e94efbeb45f3c57e77d
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2020-03-13 18:18:43 +0400

    EditorForModel

---
+++ Model/02_EditorForModel/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using _02_EditorForModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _02_EditorForModel.Models.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Product product = new Product()
+            {
+                Id = 1,
+                Name = "Test Product",
+                IsTangible = false,
+            };
+
+            return View(product);
+        }
+
+
+    }
+}
